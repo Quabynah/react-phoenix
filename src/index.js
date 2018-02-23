b40fa0dc37384c1388460332e85876694db947f5
Dennis Bilson <quabynahdennis@gmail.com>
2018-02-23 04:50:48 +0000

    Latest
    Latest update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createBrowserHistory } from 'history';
+import {
+    Router,
+    Route,
+    Switch
+} from 'react-router-dom';
+
+import '/assets/css/material-dashboard-react.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import indexRoutes from 'routes/index.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const hist = createBrowserHistory();
+
+ReactDOM.render( < App / > , <
+    Router history = { hist } >
+    <
+    Switch > {
+        indexRoutes.map((prop, key) => {
+            return ( <
+                Route path = { prop.path }
+                component = { prop.component }
+                key = { key }
+                />
+            );
+        })
+    } <
+    /Switch> <
+    /Router>, document.getElementById('root'));
 registerServiceWorker();
