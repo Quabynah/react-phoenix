c95ec4fdd0d876686212e9eece6f5355cb8a7626
Dennis Bilson <quabynahdennis@gmail.com>
2018-02-23 04:52:24 +0000

    New

--- src/index.js
+++ src/index.js
@@ -15,19 +15,17 @@ import indexRoutes from 'routes/index.jsx';
 
 const hist = createBrowserHistory();
 
-ReactDOM.render( < App / > , <
-    Router history = { hist } >
-    <
-    Switch > {
-        indexRoutes.map((prop, key) => {
-            return ( <
-                Route path = { prop.path }
-                component = { prop.component }
-                key = { key }
-                />
-            );
-        })
-    } <
-    /Switch> <
-    /Router>, document.getElementById('root'));
+ReactDOM.render( < App / > , {
+    /* <Router history={hist}>
+            <Switch>
+                {
+                    indexRoutes.map((prop,key) => {
+                        return (
+                            <Route path={prop.path} component={prop.component}  key={key}/>
+                        );
+                    })
+                }
+            </Switch>
+        </Router> */
+}, document.getElementById('root'));
 registerServiceWorker();
