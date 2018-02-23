2733707624c0a181536dad2b4375d8c50c1b689f
Dennis Bilson <quabynahdennis@gmail.com>
2018-02-23 04:34:34 +0000

    New Data
    This is my third commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyDyO2pWA3OonAXIsHrvKKkeWhVHpkI9Bvs",
+        authDomain: "phoenix-master.firebaseapp.com",
+        databaseURL: "https://phoenix-master.firebaseio.com",
+        projectId: "phoenix-master",
+        storageBucket: "phoenix-master.appspot.com",
+        messagingSenderId: "118933953659"
+    }
+};
